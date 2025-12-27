625e502280ebe98511c9dbfc239c1e188f634a18
Saifulla-boop <58859371+Saifulla-boop@users.noreply.github.com>
2025-12-27 15:40:49 +0500

    Update Root.tsx

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -2,21 +2,27 @@ import React from "react";
 import { Composition } from "remotion";
 import { Short } from "./Short";
 
-export const RemotionRoot: React.FC = () => (
-  <Composition
-    id="Short"
-    component={Short}
-    width={1080}
-    height={1920}
-    fps={30}
-    durationInFrames={30 * 12}
-    defaultProps={{
-      hook: "Почему бизнес не масштабируется",
-      videoUrl: "",
-      musicUrl: "",
-      durationSec: 12,
-      style: "premium_business",
-      emphasis: ["не масштабируется"],
-    }}
-  />
-);
+export const RemotionRoot: React.FC = () => {
+  return (
+    <>
+      <Composition
+        id="Short"
+        component={Short}
+        width={1080}
+        height={1920}
+        fps={30}
+        durationInFrames={30 * 12}
+        defaultProps={{
+          hook: "Почему нет продаж?",
+          videoUrl: "",
+          musicUrl: "",
+          durationSec: 12,
+
+          // === НОВОЕ ===
+          // Позиция текста: "top" или "center"
+          textPosition: "top",
+        }}
+      />
+    </>
+  );
+};
