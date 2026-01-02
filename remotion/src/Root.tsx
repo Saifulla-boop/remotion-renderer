d8717781eafb4dfdddc9465a98b5788978370cae
Saifulla-boop <58859371+Saifulla-boop@users.noreply.github.com>
2026-01-02 05:59:18 +0500

    Update Root.tsx

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {Composition} from "remotion";
-import {Short} from "./Short";
+import { Composition } from "remotion";
+import { Short } from "./Short";
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -11,11 +11,15 @@ export const RemotionRoot: React.FC = () => {
         width={1080}
         height={1920}
         fps={30}
-        durationInFrames={30 * 12} // дефолт 12 сек (можешь перезаписывать с сервера)
+        durationInFrames={30 * 12}
         defaultProps={{
-          videoSrc: "",
-          musicSrc: "",
+          // важно: названия должны совпадать с тем,
+          // что ты используешь в Short.tsx и что приходит с сервера
           hook: "",
+          description: "",
+          durationSec: 12,
+          videoPath: "",
+          musicPath: "",
         }}
       />
     </>
