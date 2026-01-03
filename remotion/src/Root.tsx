4b1ce35f70cfe04833a66550eef9d633261cdcde
Saifulla-boop <58859371+Saifulla-boop@users.noreply.github.com>
2026-01-03 15:03:43 +0500

    Update Root.tsx

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Composition } from "remotion";
+import { Composition, staticFile } from "remotion";
 import { Short } from "./Short";
+
+const fontCss = `
+@font-face {
+  font-family: "Montserrat";
+  src: url("${staticFile("fonts/Montserrat-Regular.ttf")}") format("truetype");
+  font-weight: 400;
+  font-style: normal;
+  font-display: swap;
+}
+@font-face {
+  font-family: "Montserrat";
+  src: url("${staticFile("fonts/Montserrat-SemiBold.ttf")}") format("truetype");
+  font-weight: 600;
+  font-style: normal;
+  font-display: swap;
+}
+`;
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      {/* ВАЖНО: никаких сетевых загрузок шрифта */}
+      <style dangerouslySetInnerHTML={{ __html: fontCss }} />
+
       <Composition
         id="Short"
         component={Short}
@@ -14,13 +34,13 @@ export const RemotionRoot: React.FC = () => {
         durationInFrames={60 * 10}
         defaultProps={{
           hook: "Почему нет продаж?",
+          description: "",
           videoUrl: "",
           musicUrl: "",
           durationSec: 10,
-
-          // === НОВОЕ ===
-          // Позиция текста: "top" или "center"
           textPosition: "top",
+          musicVolume: 0.35,
+          videoVolume: 1,
         }}
       />
     </>
